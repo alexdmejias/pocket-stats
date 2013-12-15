414c8f640b8d7d2187df69c1e88707a3e2031d78
alex mejias <alexdmejias@gmail.com>
2013-12-15 00:53:36 -0500

    small tweak to instructions

--- application/controllers/pocket.php
+++ application/controllers/pocket.php
@@ -81,7 +81,7 @@ class Pocket extends CI_Controller {
         $access_token = explode('&',$result);
         if($access_token[0]!=''){
                 echo "<h1>You've been authenticated succesfully!</h1>";
-                echo "You should write down the access_token and then add it to config.php<br>";
+                echo "You should write down the access_token and then add it to the appropiate pocket.php in the config dir.<br>";
                 echo "Your access token: ". $access_token[0];
                 echo "<br>";
                 echo "add this to config.php";
