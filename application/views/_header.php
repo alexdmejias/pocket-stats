b2fda47a31846a1d6ec79255adc1c4e69f9474f0
alex mejias <alexdmejias@gmail.com>
2013-12-17 23:34:37 -0500

    autoload url helper; cleaned up 'basic' view; including d3

--- application/views/_header.php
+++ application/views/_header.php
@@ -4,8 +4,8 @@
 	<meta charset="utf-8">
 	<title>Welcome to CodeIgniter</title>
 	<link rel="stylesheet" href="<?php echo site_url('/library/css/styles.css'); ?>">
-
+	<script src="<?php echo site_url('/bower_components/d3/d3.min.js'); ?>"></script>
 </head>
 <body>
 
-<div id="container">
+<div id="container"><?php //completed in the footer ?>
